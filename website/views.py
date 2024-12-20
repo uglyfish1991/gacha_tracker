ea7d9c00b5a30e6427d7ac86d038b032f08ea583
Katy Long <kelong1890@gmail.com>
2024-12-20 22:10:14 +0000

    list order flipped and count from last legendary added!

--- website/views.py
+++ website/views.py
@@ -6,6 +6,14 @@ from datetime import date
 
 my_view = Blueprint("my_view", __name__)
 
+def count_from_last_five_star(game):
+     counter = 0
+     for record in PullRecord.query.filter_by(game_name=game).order_by(PullRecord.id.desc()).all():
+          counter +=1
+          if record.character_rarity == 5:
+               return counter
+          
+
 @my_view.route("/")
 def home():
     form_type = request.args.get('form_type', None)
@@ -25,18 +33,21 @@ def form_select():
          game_stats = omni_info
     else:
         form_type="other"
-    record_collection = PullRecord.query.filter_by(game_name=game_stats["game_name"]).limit(10).all()
-    return render_template("form.html", form_type=form_type, game_stats = game_stats, record_collection=record_collection)
+    record_collection = PullRecord.query.filter_by(game_name=game_stats["game_name"]).order_by(PullRecord.id.desc()).limit(10).all()
+    last_five_star = count_from_last_five_star(game_stats["game_name"])
+    return render_template("form.html", form_type=form_type, game_stats = game_stats, record_collection=record_collection, last_five_star = last_five_star)
 
 @my_view.route("/records", endpoint='view_records')
 def view_records():
         record_collection = PullRecord.query.all()
+        record_collection.reverse()
         return render_template("record.html", record_collection = record_collection)
 
 @my_view.route("/records/<specific_game>")
 def view_specific_records(specific_game):
         print(specific_game)
         record_collection = PullRecord.query.filter_by(game_name=specific_game).all()
+        record_collection.reverse()
         return render_template("record.html", record_collection = record_collection)
 
 @my_view.route("/add_record", methods = ["POST"])
