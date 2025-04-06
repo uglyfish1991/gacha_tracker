5d0e69ff0fc8f33b1ad17faac34c47b550f6b02f
Katy Long <kelong1890@gmail.com>
2025-04-06 15:58:44 +0100

    env sorted out

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+post_gres_uri = os.environ.get("post_gres_uri")
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = post_gres_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import my_view
@@ -16,3 +24,5 @@ def create_app():
         db.create_all()
 
     return app
+
+    
